0ef1076dd212b6e838a41f232925a4a9871f2b48
Aobakwe-Sephiri <81358017+Aobakwe-Sephiri@users.noreply.github.com>
2022-11-11 22:54:25 +0200

    PPO.py

--- scripts/PPO.py
+++ scripts/PPO.py
@@ -1,3 +1,4 @@
+# Required Imports
 import gym
 import minihack
 from stable_baselines3 import PPO
@@ -6,8 +7,6 @@ from utilities import generate_video
 from stable_baselines3.common.callbacks import BaseCallback,CallbackList, CheckpointCallback, EvalCallback
 from stable_baselines3.common.monitor import Monitor, ResultsWriter
 from stable_baselines3.common.logger import configure
-
-# Changes Here
 from stable_baselines3.common.env_util import make_vec_env
         
 
@@ -20,32 +19,30 @@ def main():
     MODEL_SAVE_PATH = MODEL_SAVING_DIR+MODEL_NAME
     log_path = "../logs/PPO_log/"
 
-    # Changes Here (Added Parallel Environments)
     # Parallel environments
-    # Parallel environments are just an optimization trick that allows PPO to run multiple agent/environment pairs at the same time. This can greatly increase the speed at which environment steps are generated, and can also make the transitions within a batch more IID too.
+    # Parallel environments allows PPO to run multiple agents pairs at the same time. This will increase the speed at which environment steps are generated, and will make transitions within a batch more IID too.
+    # Make_vec_env is method for stacking multiple independent environments into a single environment, and use init_env from environment as import for the method
     env = make_vec_env(init_env, n_envs=4)
 
     model = PPO("MultiInputPolicy", env, verbose=1)
     
-    ## Continue With The Prev Model, Unquote below lines to load prev model
-    # PATH_TO_PREV_MODEL = TODO 
-    # model.load(PATH_TO_PREV_MODEL)
-    # model.set_env(env)
-    
     ## Checkpoint CallBack -
     ## Callback for saving a model every save freq 
     checkpoint_callback = CheckpointCallback(save_freq=1000, save_path=MODEL_SAVE_PATH)
+        
     ## EvalCallback 
     # Separate evaluation env
     eval_env = init_env()
+
     ## evaluate periodically the performance of an agent, it will save the best model 
     eval_callback = EvalCallback(eval_env, best_model_save_path=MODEL_SAVING_DIR+"best_model", log_path="../logs/PPO/results", eval_freq=500)
     callback = CallbackList([checkpoint_callback, eval_callback])
 
-    #Setting Up Configure
+    #setting up configure used For getting the results needed for the reward per episode grapgs
     new_logger = configure(log_path, ["stdout", "csv", "tensorboard"])
     model.set_logger(new_logger)
-    
+
+    # PPO models trains
     model.learn(total_timesteps=100000000,callback=callback)
     model.save()
 
